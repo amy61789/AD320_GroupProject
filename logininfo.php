0d2efab9a684e46ff0169993a99d0f131f59f2ff
Amy Funk <amy61789@outlook.com>
2018-03-13 15:49:49 -0700

    Login Code

---
+++ logininfo.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+function is_logged_in() {
+	if (!isset($_SESSION["user"]) && isset($_COOKIE["user"])) {
+		$_SESSION["user"] = $_COOKIE["user"];
+	}
+	return isset($_SESSION["user"]);
+}
+
+function logout() {
+	unset($_SESSION["user"]);
+	setcookie("user", "", 0, "/");
+}
+?>
