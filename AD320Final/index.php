1af3dc2cfe701f81e9a4d25ff8811b2f3ca49ee9
Shota Takada <34498187+Stakada@users.noreply.github.com>
2018-03-13 16:30:23 -0700

    Update index.php

--- AD320Final/index.php
+++ AD320Final/index.php
@@ -1,5 +1,14 @@
 <?php
-  include "information.php";
+    
+    $pickup = filter_input(INPUT_POST,"start");
+    $dropoff = filter_input(INPUT_POST, "end");
+    $pickup_date = filter_input(INPUT_POST, "day_start");
+    $pickup_month = filter_input(INPUT_POST, "month_start");
+    $pickup_year = filter_input(INPUT_POST, "year_start");
+    $dropoff_month= filter_input(INPUT_POST, "day_end");
+    $dropoff_date =filter_input(INPUT_POST, "month_end");
+    $dropoff_year = filter_input(INPUT_POST, "year_end");  
+    
 ?>
 
 <!doctype html>
@@ -34,80 +43,74 @@
 <nav>
 <ul>
     
-<li><a href="index.html">Rental Car</a></li>
+<li><a href="index.php">Rental Car</a></li>
 <li><a href="about.html">About Us</a></li>
 <li><a href="policy.html">Rental Policy </a></li>
+<li><a href="policy.html">My Account </a></li>
 </ul>
 </nav>
 
 <main>
-<h1>Make a Reservation:</h1>
+<h1 style="font-weight: bold">Make a Reservation:</h1>
 <table class="main">
-     
+    <form action="cars.php" method="POST"> 
         <tr>
-            <td>Pic-up Location <input type="text" name="pickup" style="width: 300px; height:25px;"></td></tr>
-    
-         
         <div class="dates">
         <fieldset class="date"> 
         <legend>Start Date </legend> 
         <label for="month_start">Month</label> 
-        <select id="month_start" 
-                name="month_start"> 
-            <option name="pickup_month" value="January">January</option>       
-            <option name="pickup_month" value="February">February</option>       
-            <option name="pickup_month" value="March">March</option>       
-            <option name="pickup_month" value="April">April</option>       
-            <option name="pickup_month" value="May">May</option>       
-            <option name="pickup_month" value="June">June</option>       
-            <option name="pickup_month" value="July">July</option>       
-            <option name="pickup_month" value="August">August</option>       
-            <option name="pickup_month" value="September">September</option>       
-            <option name="pickup_month" value="October">October</option>       
-            <option name="pickup_month" value="November">November</option>       
-            <option name="pickup_month" value="December">December</option>  
+        <select id="month_start" name="month_start" > 
+            <option value="January">January</option>       
+            <option value="February">February</option>       
+            <option value="March">March</option>       
+            <option value="April">April</option>       
+            <option value="May">May</option>       
+            <option value="June">June</option>       
+            <option value="July">July</option>       
+            <option value="August">August</option>       
+            <option value="September">September</option>       
+            <option value="October">October</option>       
+            <option value="November">November</option>       
+            <option value="December">December</option>
           </select> - 
           <label for="day_end">Day</label> 
-          <select id="day_end" 
-                  name="day_end" > 
-            <option name="pickup_date" value="1">1</option>       
-            <option name="pickup_date" value="2">2</option>       
-            <option name="pickup_date" value="3">3</option>       
-            <option name="pickup_date" value="4">4</option>       
-            <option name="pickup_date" value="5">5</option>       
-            <option name="pickup_date" value="6">6</option>       
-            <option name="pickup_date" value="7">7</option>       
-            <option name="pickup_date" value="8">8</option>       
-            <option name="pickup_date" value="9">9</option>       
-            <option name="pickup_date" value="10">10</option>       
-            <option name="pickup_date" value="11">11</option>       
-            <option name="pickup_date" value="12">12</option>       
-            <option name="pickup_date" value="13">13</option>       
-            <option name="pickup_date" value="14">14</option>       
-            <option name="pickup_date" value="15">15</option>       
-            <option name="pickup_date" value="16">16</option>       
-            <option name="pickup_date" value="17">17</option>       
-            <option name="pickup_date" value="18">18</option>       
-            <option name="pickup_date" value="19">19</option>       
-            <option name="pickup_date" value="20">20</option>       
-            <option name="pickup_date" value="21">21</option>       
-            <option name="pickup_date" value="22">22</option>       
-            <option name="pickup_date" value="23">23</option>       
-            <option name="pickup_date" value="24">24</option>       
-            <option name="pickup_date" value="25">25</option>       
-            <option name="pickup_date" value="26">26</option>       
-            <option name="pickup_date" value="27">27</option>       
-            <option name="pickup_date" value="28">28</option>       
-            <option name="pickup_date" value="29">29</option>       
-            <option name="pickup_date" value="30">30</option>       
-            <option name="pickup_date" value="31">31</option>       
+          <select id="day_end" name="day_start" > 
+            <option value="1">1</option>       
+            <option value="2">2</option>       
+            <option value="3">3</option>       
+            <option value="4">4</option>       
+            <option value="5">5</option>       
+            <option value="6">6</option>       
+            <option value="7">7</option>       
+            <option value="8">8</option>       
+            <option value="9">9</option>       
+            <option value="10">10</option>       
+            <option value="11">11</option>       
+            <option value="12">12</option>       
+            <option value="13">13</option>       
+            <option value="14">14</option>       
+            <option value="15">15</option>       
+            <option value="16">16</option>       
+            <option value="17">17</option>       
+            <option value="18">18</option>       
+            <option value="19">19</option>       
+            <option value="20">20</option>       
+            <option value="21">21</option>       
+            <option value="22">22</option>       
+            <option value="23">23</option>       
+            <option value="24">24</option>       
+            <option value="25">25</option>       
+            <option value="26">26</option>       
+            <option value="27">27</option>       
+            <option value="28">28</option>       
+            <option value="29">29</option>       
+            <option value="30">30</option>       
+            <option value="31">31</option>        
           </select> - 
           <label for="year_end">Year</label> 
-          <select id="year_end" 
-                 name="year_end" > 
-
-            <option name="pickup_year" value="2018">2018</option>
-            <option name="pickup_year" value="2019">2019</option>
+          <select id="year_end" name="year_start" > 
+            <option value="2018">2018</option>
+            <option value="2019">2019</option>
           </select> 
 
         </fieldset> 
@@ -115,77 +118,85 @@
         <fieldset class="date"> 
           <legend>End Date </legend> 
           <label for="month_end">Month</label> 
-          <select id="month_end" 
-                  name="month_end" > 
-            <option name="dropoff_month" value="January">January</option>       
-            <option name="dropoff_month" value="February">February</option>       
-            <option name="dropoff_month" value="March">March</option>       
-            <option name="dropoff_month" value="April">April</option>       
-            <option name="dropoff_month" value="May">May</option>       
-            <option name="dropoff_month" value="June">June</option>       
-            <option name="dropoff_month" value="July">July</option>       
-            <option name="dropoff_month" value="August">August</option>       
-            <option name="dropoff_month" value="September">September</option>       
-            <option name="dropoff_month" value="October">October</option>       
-            <option name="dropoff_month" value="November">November</option>       
-            <option name="dropoff_month" value="December">December</option>  
-          </select> - 
+          <select id="month_end"name="month_end" > 
+            <option value="January">January</option>       
+            <option value="February">February</option>       
+            <option value="March">March</option>       
+            <option value="April">April</option>       
+            <option value="May">May</option>       
+            <option value="June">June</option>       
+            <option value="July">July</option>       
+            <option value="August">August</option>       
+            <option value="September">September</option>       
+            <option value="October">October</option>       
+            <option value="November">November</option>       
+            <option value="December">December</option>  
+          </select> -
           <label for="day_end">Day</label> 
-          <select id="day_end" 
-                  name="day_end" > 
-            <option name="dropoff_date" value="1">1</option>       
-            <option name="dropoff_date" value="2">2</option>       
-            <option name="dropoff_date" value="3">3</option>       
-            <option name="dropoff_date" value="4">4</option>       
-            <option name="dropoff_date" value="5">5</option>       
-            <option name="dropoff_date" value="6">6</option>       
-            <option name="dropoff_date" value="7">7</option>       
-            <option name="dropoff_date" value="8">8</option>       
-            <option name="dropoff_date" value="9">9</option>       
-            <option name="dropoff_date" value="10">10</option>       
-            <option name="dropoff_date" value="11">11</option>       
-            <option name="dropoff_date" value="12">12</option>       
-            <option name="dropoff_date" value="13">13</option>       
-            <option name="dropoff_date" value="14">14</option>       
-            <option name="dropoff_date" value="15">15</option>       
-            <option name="dropoff_date" value="16">16</option>       
-            <option name="dropoff_date" value="17">17</option>       
-            <option name="dropoff_date" value="18">18</option>       
-            <option name="dropoff_date" value="19">19</option>       
-            <option name="dropoff_date" value="20">20</option>       
-            <option name="dropoff_date" value="21">21</option>       
-            <option name="dropoff_date" value="22">22</option>       
-            <option name="dropoff_date" value="23">23</option>       
-            <option name="dropoff_date" value="24">24</option>       
-            <option name="dropoff_date" value="25">25</option>       
-            <option name="dropoff_date" value="26">26</option>       
-            <option name="dropoff_date" value="27">27</option>       
-            <option name="dropoff_date" value="28">28</option>       
-            <option name="dropoff_date" value="29">29</option>       
-            <option name="dropoff_date" value="30">30</option>       
-            <option name="dropoff_date" value="31">31</option>       
+          <select id="day_end" name="day_end" > 
+            <option value="1">1</option>       
+            <option value="2">2</option>       
+            <option value="3">3</option>       
+            <option value="4">4</option>       
+            <option value="5">5</option>       
+            <option value="6">6</option>       
+            <option value="7">7</option>       
+            <option value="8">8</option>       
+            <option value="9">9</option>       
+            <option value="10">10</option>       
+            <option value="11">11</option>       
+            <option value="12">12</option>       
+            <option value="13">13</option>       
+            <option value="14">14</option>       
+            <option value="15">15</option>       
+            <option value="16">16</option>       
+            <option value="17">17</option>       
+            <option value="18">18</option>       
+            <option value="19">19</option>       
+            <option value="20">20</option>       
+            <option value="21">21</option>       
+            <option value="22">22</option>       
+            <option value="23">23</option>       
+            <option value="24">24</option>       
+            <option value="25">25</option>       
+            <option value="26">26</option>       
+            <option value="27">27</option>       
+            <option value="28">28</option>       
+            <option value="29">29</option>       
+            <option value="30">30</option>       
+            <option value="31">31</option>       
           </select> - 
           <label for="year_end">Year</label> 
-          <select id="year_end" 
-                 name="year_end" > 
-
-            <option name="dropoff_year" value="2018">2018</option>
-            <option name="dropoff_year" value="2019">2019</option>
+          <select id="year_end" name="year_end" > 
+            <option value="2018">2018</option>
+            <option value="2019">2019</option>
           </select> 
 
         </fieldset> 
-    </div>
-    <tr><td>Return Location: <input type="text" name="dropoff" style="width: 300px;  height:25px;"></td><tr>
-     
- 
- 
-<form action="cars.php">
-<tr><td><input type="submit" value="Pick a car"></td></tr>
-</form>
-
+           
+    </form>
 </table>
-</main>
-<footer>
+        <input type="submit" value="submit" name="Pick Your Car">
+    </div>
+    <h2 style='font-weight:bold; padding:20px; '>Pick your car size</h2>
+
+    <div class="row">
+        <div class="column1"><img src="images/compact1.1.jpg" alt="compact car1"></div>
+        <div class="column1"><img src="images/compact2.1.jpg" alt="compact car2"></div>
+        <div class="column1"><img src="images/mid1.1.jpg" alt="mid car1"></div>
+        <div class="column1"><img src="images/mid2.1.jpg" alt="mid car2"></div>
+    </div>
+    <div class="row">
+        <div class="column"><input type="radio" name="car" value="Prius">Compact</div>
+        <div class="column col2"><input type="radio" name="car" value="Civic">Standard</div>
+        <div class="column col3"><input type="radio" name="car" value="Camry">Full Size</div>
+        <div class="column col4"><input type="radio" name="car" value="Accord">SUV</div>
+
+    </div>
+ 
+    <div><input type="submit" value="Go to your cart"></div>
+     
+ </main>
 <ul>
 <li>Copyright 2017, &copy;</li>
 <li>All Rights Reserved</li>
