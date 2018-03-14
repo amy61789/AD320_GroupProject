29f303ee4966155065bc69166ef4272f030c9b97
Shota Takada <34498187+Stakada@users.noreply.github.com>
2018-03-14 14:45:22 -0700

    Update login.php

--- final/login.php
+++ final/login.php
@@ -24,13 +24,13 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
         } 
 	$email = $_POST["email"];
 	$pass = $_POST["password"];
-        $query = "SELECT * from customer WHERE email=shota@gmail.com";
-	//$query = "SELECT Pas from Credentials WHERE Username='" . $user . "'";
+        $query = "SELECT * from customer WHERE email='$email'";
 	$result = mysqli_query($conn, $query) or die(mysql_error());
 	$row = mysqli_fetch_assoc($result);
         var_dump($row);
 	if ($pass == $row["password"]) {
-		$_SESSION["email"] = $user;
+		$_SESSION["email"] = $email;
+                $_SESSION["pass"] =$pass;
 		if (isset($_POST["remember"])) {
 			setcookie("email", $email, time() + 60*60*24, "/");
 		}
@@ -40,19 +40,8 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
 	}
 }
 ?>
-    <style>
-        form{
-            width:500px;
-            text-align: center;
-            background: rgba(100, 100, 100, 0.5); 
-            
-        }
-        
-        
-        
-    </style>
+
 <!-- not logged in -->
-<h2>Log in here</h2>
 <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
 	Email: <input type="text" name="email" /><br />
 	Password: <input type="password" name="password" /><br />
