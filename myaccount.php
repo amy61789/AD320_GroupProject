4c01b73ae7ef027757606e2cf35dddbd6035e7c2
bradleyshao <34496812+bradleyshao@users.noreply.github.com>
2018-03-13 19:26:31 -0700

    Add files via upload

--- myaccount.php
+++ myaccount.php
@@ -1,33 +1,24 @@
 <?php
-$servername = "localhost";
-        $username = "icoolsho_afunk";
-        $password = "$!991-26-3507";
-        $dbname = "icoolsho_afunk";
-
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-
+include "login.php";
+$email= 'bradley.shao@gmail.com';
 $sql = "SELECT email FROM customer WHERE email = $email ";
 
-$result = mysqli_query($conn, $sql);
+ $result = mysqli_query($conn, $sql);
 
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "username: " . $row["email"].  "<br>";
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "username: " . $row["email"] . "<br>";
+        }
+    } else {
+        echo "0 results";
     }
-} else {
-    echo "0 results";
-}
 
-mysqli_close($conn);
-?>
+    mysqli_close($conn);
+    ?>
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
